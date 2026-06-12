50c9d5169fe89486e9819492d2c85ccb189c9293
Carlos27Spec <kaduvieira27@gmail.com>
2026-06-12 19:17:25 -0100

    Update script.js

--- script.js
+++ script.js
@@ -318,7 +318,7 @@ function calcularRegraDeTres() {
 
   const x = (b * c) / a;
 
-  const xFormatado = x.toLocaleString("pt-BR", { maximumFractionDigits: 2 });
+  const xFormatado = x.toFixed(2).replace(".", ",").replace(/,00$/, "");
 
   campoX.value = xFormatado;
 
